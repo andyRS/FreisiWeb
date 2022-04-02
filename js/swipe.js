0824945c20749cc1437f85adada327425b1b0628
andyrs <andy337@hotmail.es>
2022-04-02 03:34:12 -0400

    update

--- js/swipe.js
+++ js/swipe.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener('load', function(){
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 4,
@@ -15,22 +14,4 @@ window.addEventListener('load', function(){
           prevEl: ".swiper-button-prev",
         },
       });  
-=======
-window.addEventListener('load', function(){
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        slidesPerGroup: 1,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-          },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });  
->>>>>>> 5b6543f944f8beedfc5f3459e2ef0e023d665334
 })
