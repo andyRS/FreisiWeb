f571b41900f50ff7a270f72b59c5d50cc8d42e89
andyrs <andy337@hotmail.es>
2022-04-02 03:16:49 -0400

    ultimate update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-window.addEventListener('load',function(){
-    
+window.addEventListener('load', function () {
+
     /** Indicador de menu*/
 
     const secciones = document.querySelectorAll('.section')
     const menuItems = document.querySelectorAll('.nav-menu-item')
 
-const funcionObserverser = entries =>{
-    entries.forEach(entry => {
-        if(entry.isInteresting){
-            const itemActual = Array.from(menuItems).find(item => item.dataset.url === entry.target.id)
-            itemActual.classList.add('active')
-        }
-    });
-}
+    const funcionObserverser = entries => {
+        entries.forEach(entry => {
+            if (entry.isInteresting) {
+                const itemActual = Array.from(menuItems).find(item => item.dataset.url === entry.target.id)
+                itemActual.classList.add('active')
+            }
+        });
+    }
 
     const observer = new IntersectionObserver(funcionObserverser, {
         root: null,
@@ -20,8 +20,39 @@ const funcionObserverser = entries =>{
         threshold: 0.8
     })
 
-    secciones.forEach(seccion =>observer.observe(seccion))
+    secciones.forEach(seccion => observer.observe(seccion))
+
+    //Header Scroll
+
+    var menu = document.getElementById('menu')
+    let logoMenu = document.getElementById('logo-menu')
+    let link1 = document.getElementById('link-1')
+    let link2 = document.getElementById('link-2')
+    let link3 = document.getElementById('link-3')
+    let link4 = document.getElementById('link-4')
+
+
+
+    window.addEventListener('scroll', () => {
+        var scroll = window.scrollY
+        if (scroll > 10) {
+            menu.style.backgroundColor = '#121212'
+            logoMenu.style.color = '#fff'
+            logoMenu.style.left = "100px";
+            link1.style.color ="#fff"
+            link2.style.color ="#fff"
+            link3.style.color ="#fff"
+            link4.style.color ="#fff"
+
+        } else {
+            menu.style.backgroundColor = '#fff'
+            logoMenu.style.color = '#444444'
+            link1.style.color ="#444444"
+            link2.style.color ="#444444"
+            link3.style.color ="#444444"
+            link4.style.color ="#444444"
+        }
+    })
 
 
 })
-
