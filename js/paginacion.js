8808d7df1ec9d0b89d3f5e9e72449b2e0cccd500
andyRS <andy337@hotmail.es>
2022-04-02 01:22:50 -0400

    Delete js directory

--- js/paginacion.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load',()=>{
-    var next = document.querySelector('.page-link-next');
-    var contenido1 = document.querySelector('#content')
-        next.addEventListener('click',function() {
-            next.style.display='flex';
-            contenido1.style.display='none';
-    })
-})
