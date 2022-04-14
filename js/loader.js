261c57fe2be266c8b4ef62c8532b318e1ac21ce4
andyRS <andy337@hotmail.es>
2022-04-14 18:54:52 -0400

    update

---
+++ js/loader.js
@@ -0,0 +1,9 @@
+'use strict'
+
+window.addEventListener('load', ()=>{
+    const contenedorLoader = document.querySelector('#pre-load');
+
+    contenedorLoader.style.opacity= 0;
+    contenedorLoader.style.visibility = 'hidden';
+
+});
