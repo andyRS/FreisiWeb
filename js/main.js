434b9ef76965064ea1282a1cd24c44a20de3cd2c
andyRS <andy337@hotmail.es>
2022-04-20 19:52:18 -0400

    update

--- js/main.js
+++ js/main.js
@@ -61,5 +61,33 @@ window.addEventListener('load', function () {
         }
     })
 
-})
+    //Menu Hamburgesa
+    document.querySelector(".bars__menu").addEventListener("click", animateBars);
 
+
+
+    var line1__bars = document.querySelector(".line1__bars-menu");
+    var line2__bars = document.querySelector(".line2__bars-menu");
+    var line3__bars = document.querySelector(".line3__bars-menu");
+    var container_menu = document.querySelector(".nav-menu")
+    var menu = document.querySelector('#menu');
+    var btnMenuHamburg = document.querySelector(".btn-menu-hamb");
+    if(btnMenuHamburg){
+        btnMenuHamburg.addEventListener("click",()=>{
+            menu.classList.toggle("mostrar-hei");
+        })
+    }
+
+
+
+    function animateBars() {
+        line1__bars.classList.toggle("activeline1__bars-menu");
+        line2__bars.classList.toggle("activeline2__bars-menu");
+        line3__bars.classList.toggle("activeline3__bars-menu");
+        container_menu.classList.toggle("menu__active");
+    }
+
+
+
+
+})
